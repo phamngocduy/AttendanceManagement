cdedea77945c3ebedf0c49e6f734b2e5a19eb4a1
Duy Khấu <duykhau1@vanlanguni.vn>
2019-02-20 11:06:29 +0700

    Fix change language function

--- WebApplication/App_Start/RouteConfig.cs
+++ WebApplication/App_Start/RouteConfig.cs
@@ -12,12 +12,13 @@ namespace WebApplication
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
 			routes.MapRoute(
-	name: "DefaultLocalized",
-	url: "{lang}/{controller}/{action}/{id}",
-	constraints: new { lang = @"(\w{2})|(\w{2}-\w{2})" },   // en or en-US
-	defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-);
+				name: "DefaultLocalized",
+				url: "{lang}/{controller}/{action}/{id}",
+				constraints: new { lang = @"(\w{2})|(\w{2}-\w{2})" },   // en or en-US
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
