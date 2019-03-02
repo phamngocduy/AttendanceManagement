4cb28834beb1348cc5d916159836d58310f6c57c
minhvo256 <vocongminh256@gmail.com>
2019-03-02 13:01:14 +0700

    UnitTest-View My Group

---
+++ WebApplication.Tests/Controllers/GroupControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication;
+using WebApplication.Controllers;
+using WebApplication.Models;
+
+namespace WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class GroupControllerTest
+    {
+        [TestMethod]
+        public void TestViewMyGroup()
+        {
+            var controller = new GroupController();
+            var result = controller.Index() as ViewResult;
+            var db = new cap21t4Entities();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<Group>));
+            Assert.AreEqual(db.Groups.Where(x => x.GroupParent == null).Count(), ((List<Group>)result.Model).Count);
+        }
+    }
+}
