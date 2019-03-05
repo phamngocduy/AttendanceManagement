1561d4f4da92a5539615b41c669bece921391aca
minhvo256 <vocongminh256@gmail.com>
2019-03-05 10:14:49 +0700

    UnitTest-Edit Group

--- WebApplication.Tests/Controllers/GroupControllerTest.cs
+++ WebApplication.Tests/Controllers/GroupControllerTest.cs
@@ -9,6 +9,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebApplication;
 using WebApplication.Controllers;
 using WebApplication.Models;
+using System.Transactions;
 
 namespace WebApplication.Tests.Controllers
 {
@@ -26,5 +27,171 @@ namespace WebApplication.Tests.Controllers
             Assert.IsInstanceOfType(result.Model, typeof(List<Group>));
             Assert.AreEqual(db.Groups.Where(x => x.GroupParent == null).Count(), ((List<Group>)result.Model).Count);
         }
+        [TestMethod]
+        public void TestViewEditGroup()
+        {
+            var controller = new GroupController();
+            string id = "1";
+
+            var viewResult = controller.Edit(id) as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+        }
+
+        [TestMethod]
+        public void TestEditGroupSuccessfully()
+        {
+
+            //Arange
+            var controller = new GroupController();
+            var user = new Group();
+            var db = new cap21t4Entities();
+            Group edit = db.Groups.First();
+            edit.GroupName = "Nhóm 1.5";
+            edit.GroupDescription = "Mobile App";
+
+            using (var scope = new TransactionScope())
+            {
+
+                var result1 = controller.Edit(db.Groups.First().ID.ToString()) as ViewResult;
+                Assert.IsNotNull(result1);
+
+            }
+
+        }
+        [TestMethod]
+        public void TestEditGroupUnSuccessfullyWitEmptyName()
+        {
+
+            //Arange
+            var controller = new GroupController();
+            var user = new Group();
+            var db = new cap21t4Entities();
+            Group edit = db.Groups.First();
+            edit.GroupName = "";
+            edit.GroupDescription = "Mobile App";
+
+            using (var scope = new TransactionScope())
+            {
+
+                var result1 = controller.Edit(db.Groups.First().ID.ToString()) as ViewResult;
+                //Assert.IsNotNull(result1);
+                System.Diagnostics.Trace.WriteLine("Error data format, please try again");
+            }
+        }
+
+        [TestMethod]
+        public void TestEditGroupUnSuccessfullyWithSpecialSymbolName()
+        {
+
+            //Arange
+            var controller = new GroupController();
+            var user = new Group();
+            var db = new cap21t4Entities();
+            Group edit = db.Groups.First();
+            edit.GroupName = "&^%$%$";
+            edit.GroupDescription = "Mobile App";
+
+            using (var scope = new TransactionScope())
+            {
+
+                var result1 = controller.Edit(db.Groups.First().ID.ToString()) as ViewResult;
+                Assert.IsNotNull(result1);
+
+                System.Diagnostics.Trace.WriteLine("Error data format, please try again");
+
+            }
+        }
+        [TestMethod]
+        public void TestEditGroupUnSuccessfullyWithBlankSpaceName()
+        {
+
+            //Arange
+            var controller = new GroupController();
+            var user = new Group();
+            var db = new cap21t4Entities();
+            Group edit = db.Groups.First();
+            edit.GroupName = " ";
+            edit.GroupDescription = "Mobile App";
+
+            using (var scope = new TransactionScope())
+            {
+
+                var result1 = controller.Edit(db.Groups.First().ID.ToString()) as ViewResult;
+                Assert.IsNotNull(result1);
+
+                System.Diagnostics.Trace.WriteLine("Error data format, please try again");
+
+            }
+        }
+        [TestMethod]
+        public void TestEditGroupUnSuccessfullyWithEmptyDescription()
+        {
+
+            //Arange
+            var controller = new GroupController();
+            var user = new Group();
+            var db = new cap21t4Entities();
+            Group edit = db.Groups.First();
+            edit.GroupName = "Nhóm 1.5";
+            edit.GroupDescription = "";
+
+            using (var scope = new TransactionScope())
+            {
+
+                var result1 = controller.Edit(db.Groups.First().ID.ToString()) as ViewResult;
+                Assert.IsNotNull(result1);
+
+                System.Diagnostics.Trace.WriteLine("Error data format, please try again");
+
+            }
+        }
+        [TestMethod]
+        public void TestEditGroupUnSuccessfullyWithSymbolDescription()
+        {
+
+            //Arange
+            var controller = new GroupController();
+            var user = new Group();
+            var db = new cap21t4Entities();
+            Group edit = db.Groups.First();
+            edit.GroupName = "Nhóm 1.5";
+            edit.GroupDescription = "&^%$%$";
+
+            using (var scope = new TransactionScope())
+            {
+
+                var result1 = controller.Edit(db.Groups.First().ID.ToString()) as ViewResult;
+                Assert.IsNotNull(result1);
+
+                System.Diagnostics.Trace.WriteLine("Error data format, please try again");
+
+            }
+        }
+        [TestMethod]
+        public void TestEditGroupUnSuccessfullyWithBlankSpaceDescription()
+        {
+
+            //Arange
+            var controller = new GroupController();
+            var user = new Group();
+            var db = new cap21t4Entities();
+            Group edit = db.Groups.First();
+            edit.GroupName = "Nhóm 1.5";
+            edit.GroupDescription = " ";
+
+            using (var scope = new TransactionScope())
+            {
+
+                var result1 = controller.Edit(db.Groups.First().ID.ToString()) as ViewResult;
+                Assert.IsNotNull(result1);
+
+                System.Diagnostics.Trace.WriteLine("Error data format, please try again");
+
+            }
+        }
     }
 }
+
+    
+
