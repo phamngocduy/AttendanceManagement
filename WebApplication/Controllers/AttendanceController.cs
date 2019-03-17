defb837992f9a1d4d2717addc9766c0a2b93ee68
Nguyen Tan Ninh <nguyentanninh123@gmail.com>
2019-03-17 09:29:48 +0700

    Attendance

--- WebApplication/Controllers/AttendanceController.cs
+++ WebApplication/Controllers/AttendanceController.cs
@@ -13,5 +13,9 @@ namespace WebApplication.Controllers
         {
             return View();
         }
+        public ActionResult manageClass()
+        {
+            return View();
+        }
     }
 }
