f1138a31c56ec797edee672a68360d90eae4ddfa
Duy Khấu <duykhau1@vanlanguni.vn>
2019-04-05 15:45:26 +0700

    All User and role

---
+++ WebApplication/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+	public class UserController : Controller
+	{
+		cap21t4Entities db = new cap21t4Entities();
+		// GET: User
+		public ActionResult Index()
+		{
+			var allUser = db.Users.ToList();
+			return View(allUser);
+		}
+
+		public ActionResult UserRole()
+		{
+			var memberlist = db.Users.Where(x => x.AspNetUser.AspNetRoles.Count() > 0);
+			List<User> users = memberlist.ToList();
+			List<UserRoleView> userRole = new List<UserRoleView>();
+			//1 nguoi 1 role
+			//foreach (var item in users)
+			//{
+			//	UserRoleView u = new UserRoleView();
+			//	u.Email = item.Email;
+			//	u.FullName = item.FullName;
+			//	u.Avatar = item.AvatarBase64;
+			//	var uRole = (from role in db.AspNetRoles
+			//				 from user in role.AspNetUsers.Where(x => x.Id == item.UserID)
+			//				 select role).First();
+			//	u.Role = uRole.Name;
+			//	userRole.Add(u);
+			//}
+
+
+			//1 nguoi 2 role
+			foreach (var item in users)
+			{
+				var uRole = (from role in db.AspNetRoles
+							 from user in role.AspNetUsers.Where(x => x.Id == item.UserID)
+							 select role);
+
+				if (uRole.Count() > 1)
+				{
+					foreach (var role in uRole)
+					{
+						UserRoleView u = new UserRoleView();
+						u.Email = item.Email;
+						u.FullName = item.FullName;
+						u.Avatar = item.AvatarBase64;
+						u.Role = role.Name;
+						userRole.Add(u);
+
+					}
+
+				}
+				else
+				{
+					UserRoleView u = new UserRoleView();
+					u.Email = item.Email;
+					u.FullName = item.FullName;
+					u.Avatar = item.AvatarBase64;
+					u.Role = uRole.First().Name;
+					userRole.Add(u);
+				}
+			}
+			return View(userRole);
+		}
+		public ActionResult CreateNewRole()
+		{
+			return View();
+		}
+	}
+}
---
+++ WebApplication/Models/UserRoleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+	public class UserRoleView
+	{
+		public string FullName { get; set; }
+		public string Email { get; set; }
+		public string Avatar { get; set; }
+		public string Role { get; set; }
+	}
+}
